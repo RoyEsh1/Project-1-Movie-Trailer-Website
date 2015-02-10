5a8738f9698332023bbf7797ef47e632af0e7a4b
RoyEsh1 <royeshel@gmail.com>
2015-02-10 12:57:41 +0000

    Create media.py

---
+++ media.py
@@ -0,0 +1,9 @@
+class Movie:
+    
+    """ Movie Class: defining movie properities, such as .title, .poster and .trailer """
+    
+    def __init__(self, movie_title, movie_story, movie_poster, movie_trailer):
+        self.title = movie_title
+        self.story = movie_story
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
